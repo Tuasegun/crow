055b2304c888c301c9316c25d5c95adc7fd1a1b4
tuasegun <tuase360@gmail.com>
2020-11-20 10:03:47 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import {Route, Switch, Redirect} from 'react-router-dom';
 import {Component} from 'react';
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component' ;
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import CheckoutPage from './pages/checkout/checkout.component';
 
@@ -54,10 +52,8 @@ class App extends Component{
         <div>
           <Header/>
           <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/' component={ShopPage} />
           <Route exact path = '/checkout'  component ={CheckoutPage} />
-          <Route exact  path='/signin' render={()=> currentUser ?(<Redirect to='/'/> ): (<SignInAndSignUpPage/>)} />
           </Switch>
           
         </div>
