0590a79283cdbb8532d22ca60b624a7847a56559
tuasegun <tuase360@gmail.com>
2020-11-20 19:23:38 +0000

    Updates

---
+++ precache-manifest.51b3f9816ed16414e921c69388669e92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef67864f690ebeb21ae5e597a857173",
+    "url": "/crown/index.html"
+  },
+  {
+    "revision": "17c15c0eb4b1f0241732",
+    "url": "/crown/static/css/main.3ac2794e.chunk.css"
+  },
+  {
+    "revision": "765183122e7f4b3a7679",
+    "url": "/crown/static/js/2.4103ea01.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crown/static/js/2.4103ea01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c15c0eb4b1f0241732",
+    "url": "/crown/static/js/main.81e72b52.chunk.js"
+  },
+  {
+    "revision": "162b7935e58308294911",
+    "url": "/crown/static/js/runtime-main.54654592.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crown/static/media/shopping-bag.30f07071.svg"
+  }
+]);
